275afbac6d47d806642351b4053785123f45a076
DayoOliyide <dayo.oliyide@gmail.com>
2016-07-05 22:39:22 +0100

    WIP - environmentMap and Resolvables functions

--- core/core.go
+++ core/core.go
@@ -2,7 +2,40 @@ package core
 
 import (
 	"regexp"
+	"os"
+	"strings"
 )
+
+func environmentMap() map[string]string {
+	envs := os.Environ()
+	envMap := make(map[string]string)
+	for _, envString := range envs  {
+		kv := strings.SplitN(envString, "=", 2)
+		envMap[kv[0]] = kv[1]
+		}
+	return envMap
+}
+
+func Resolvables(template string) []string {
+	reg := regexp.MustCompile("%%[^%]+%%")
+	resolvAll := reg.FindAllString(template, -1)
+	//why golang, why no sets ?!! why ????
+	resolvMap := make(map[string]string)
+	for _, resolv := range resolvAll {
+		if _, present := resolvMap[resolv]; !present {
+			resolvMap[resolv] = resolv
+		}
+	}
+
+	keys := make([]string, len(resolvMap))
+	i := 0
+	for k, _ := range resolvMap {
+		keys[i] = k
+		i++
+	}
+
+	return keys
+}
 
 func OutfileName(s string) (replaced string) {
 	reg := regexp.MustCompile("\\.tmpl$")
@@ -12,3 +45,13 @@ func OutfileName(s string) (replaced string) {
 	}
 	return replaced
 }
+
+/*
+func ResolveFileTemplate(envMap map[string]string, templateFile, outfile string) {
+	file, fileErr := ioutil.ReadFile(templateFile)
+	if fileErr != nil {
+		panic(fileErr)
+	}
+
+
+}*/
--- core/core_test.go
+++ core/core_test.go
@@ -1,6 +1,10 @@
 package core
 
-import "testing"
+import (
+	"testing"
+	"os"
+	"reflect"
+)
 
 
 func TestOutfileName(t *testing.T) {
@@ -17,6 +21,49 @@ func TestOutfileName(t *testing.T) {
 		got := OutfileName(c.in)
 		if got != c.want {
 			t.Errorf("OutfileName(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
+
+func TestEnvironmentMap(t *testing.T)  {
+	cases := []struct {
+		envKey, envVal, expectedVal string
+	}{
+		{"AMALGO_TEST_ENV_1", "value1", "value1"},
+		{"AMALGO_TEST_ENV_2", "value2=value2",  "value2=value2"},
+		{"AMALGO_TEST_ENV_3", "/some/file/path",  "/some/file/path"},
+		{"AMALGO_TEST_ENV_4", "space seperated value",  "space seperated value"},
+		{"AMALGO_TEST_ENV_5", "comma,seperated,value",  "comma,seperated,value"},
+		{"AMALGO_TEST_ENV_6", "semicolon;seperated;value",  "semicolon;seperated;value"},
+	}
+	for _, c := range cases {
+		os.Setenv(c.envKey, c.envVal)
+		envMap := environmentMap()
+		os.Unsetenv(c.envKey)
+		actualVal, present := envMap[c.envKey]
+		if !present || actualVal != c.expectedVal {
+			t.Errorf("Environment value is %q, expected value is %q. Got %q",
+			c.envVal, c.expectedVal, actualVal)
+		}
+	}
+}
+
+func TestResolvables(t *testing.T) {
+	cases := []struct {
+		input string; expectedVal []string
+	}{
+		{"%%TestCase1%%", []string{"%%TestCase1%%"}},
+		{"%TestCase2%", []string{}},
+		{"%%TestCase3%%blahblah%NONO%", []string{"%%TestCase3%%"}},
+		{"%%TestCase4A%%\n\tblah%%TestCase4B%%blahblah\n  %%TestCase4C%%", []string{"%%TestCase4A%%",
+			                                                                    "%%TestCase4B%%",
+		                                                                            "%%TestCase4C%%"}},
+	}
+	for _, c := range cases {
+		actualVal := Resolvables(c.input)
+		if !reflect.DeepEqual(c.expectedVal, actualVal) {
+			t.Errorf("Provided Template -> %q Expected value -> %q Actual value -> %q", c.input,
+				c.expectedVal, actualVal)
 		}
 	}
 }
